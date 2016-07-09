828388a6b46f578a19288d893c07936cf6f8ac46
SteveC <stevechenweb@gmail.com>
2016-07-08 21:56:23 -0400

    Add todo lists. Add todo items

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :todo_lists do
+    resources :todo_items
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "todo_lists#index"
 end
